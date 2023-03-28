c7ee6b8c0080cf4775667ba51098624abf18a419
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-03-28 00:06:43 -0300

    Até a aula 109
    Buscas já são realizadas pelo título e descrição

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_list_or_404
+from django.http.response import Http404
+from django.db.models import Q
 # from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -31,4 +33,18 @@ def recipe(request, id):
 
 
 def search(request):
-    return render(request, 'recipes/pages/search.html')
+    search_term = request.GET.get('q').strip()
+
+    if not search_term:
+        raise Http404()
+
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
+    })
