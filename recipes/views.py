f8ff52aeca4e835595500f186ad08d84a504e2b1
Thiago Freitas <tfreitasdevv@gmail.com>
2023-03-29 16:08:48 -0300

    Até aula 119. CSS
    Antes de tentativa de mover CSS para global

--- recipes/views.py
+++ recipes/views.py
@@ -1,30 +1,53 @@
-from django.shortcuts import render, get_list_or_404
-from django.http.response import Http404
+from django.core.paginator import Paginator
 from django.db.models import Q
-# from utils.recipes.factory import make_recipe
-from .models import Recipe
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination_range
+
+from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
 def category(request, category_id):
-    recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id'))
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
-        'title': f'{recipes[0].category.name}',
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        pk=id, is_published=True).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
@@ -33,7 +56,7 @@ def recipe(request, id):
 
 
 def search(request):
-    search_term = request.GET.get('q').strip()
+    search_term = request.GET.get('q', '').strip()
 
     if not search_term:
         raise Http404()
@@ -44,7 +67,6 @@ def search(request):
             Q(description__icontains=search_term),
         ),
         is_published=True
-
     ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', {
--- utils/pagination.py
+++ utils/pagination.py
@@ -3,14 +3,13 @@ import math
 
 def make_pagination_range(
     page_range,
-    qty_paginas,
     qty_pages,
     current_page,
 ):
-    return [1, 2, 3, 4]
     middle_range = math.ceil(qty_pages / 2)
     start_range = current_page - middle_range
     stop_range = current_page + middle_range
+    total_pages = len(page_range)
 
     start_range_offset = abs(start_range) if start_range < 0 else 0
 
@@ -18,4 +17,18 @@ def make_pagination_range(
         start_range = 0
         stop_range += start_range_offset
 
-    return page_range[start_range:stop_range]
+    if stop_range >= total_pages:
+        start_range = start_range - abs(total_pages - stop_range)
+
+    pagination = page_range[start_range:stop_range]
+    return {
+        'pagination': pagination,
+        'page_range': page_range,
+        'qty_pages': qty_pages,
+        'current_page': current_page,
+        'total_pages': total_pages,
+        'start_range': start_range,
+        'stop_range': stop_range,
+        'first_page_out_of_range': current_page > middle_range,
+        'last_page_out_of_range': stop_range < total_pages,
+    }
