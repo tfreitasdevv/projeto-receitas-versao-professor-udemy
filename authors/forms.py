e86e0168de420d385d1f49614c140a77a8c587bf
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-04-02 00:15:50 -0300

    Até aula 134. Ainda em Forms.

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,28 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['first_name']
+        labels = {
+            'first_name': 'Primeiro Nome',
+            'last_name': 'Sobrenome',
+            'username': 'Nome de Usuário',
+            'email': 'E-mail',
+            'password': 'Senha',
+        }
+        help_texts = {
+            'email': 'O e-mail deve ser válido',
+        }
+        error_messages = {
+            'username': {
+                'required': 'Este campo é de preenchimento obrigatório',
+                'invalid': 'O campo é inválido'
+            },
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Digite aqui seu nome de usuário'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Insira sua senha'
+            }),
+        }
