c428344caec68d0b5229a84ea8ac1d5c5fa96673
Thiago Freitas <tfreitasdevv@gmail.com>
2023-04-10 18:17:08 -0300

    Até aula 161. Fim de Forms
    Fim da Seção 13: Forms

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -42,13 +44,51 @@ def register_create(request):
         messages.success(request, 'Seu usuário foi criado, faça o login')
 
         del (request.session['register_form_data'])
+        return redirect(reverse('authors:login'))
 
     return redirect('authors:register')
 
 
 def login_view(request):
-    return render(request, 'authors/pages/login.html')
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Login feito com sucesso.')
+            login(request, authenticated_user)
+            return redirect(login_url)
+
+        messages.error(request, 'Credenciais inválidas')
+        return redirect(login_url)
+
+    messages.error(request, 'Erro ao validar dados do formulário')
+    return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
