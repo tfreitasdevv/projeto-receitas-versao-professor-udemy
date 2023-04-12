40a83789e8f40edd480e280ba5eb456e21144b18
Thiago Freitas <tfreitasdevv@gmail.com>
2023-04-12 16:36:42 -0300

    Até aula 163.
    Antes de tentar remover o db.sqlite3 do git

---
+++ utils/browser.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from pathlib import Path
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+ROOT_PATH = Path(__file__).parent.parent
+CHROMEDRIVER_NAME = 'chromedriver.exe'
+CHROMEDRIVER_PATH = ROOT_PATH/'bin'/CHROMEDRIVER_NAME
+
+
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=str(CHROMEDRIVER_PATH))
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
+
+
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('https://www.google.com')
+    sleep(5)
