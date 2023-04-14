1198f59b6f6b6c2fdf736918d55c508fc200d38a
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-04-13 23:07:27 -0300

    Até aula 183

--- authors/views.py
+++ authors/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from recipes.models import Recipe
 
 # Create your views here.
 
@@ -96,4 +97,12 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+
+    return render(request, 'authors/pages/dashboard.html', {
+        'recipes': recipes,
+    })
