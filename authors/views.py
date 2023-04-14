99fdd03edeaaac7300133ff92adcf814b377f820
Thiago Freitas <tfreitasdevv@gmail.com>
2023-04-14 17:59:42 -0300

    Até aula 188

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from recipes.models import Recipe
+from authors.forms.recipe_form import AuthorRecipeForm
 
 # Create your views here.
 
@@ -105,4 +106,37 @@ def dashboard(request):
 
     return render(request, 'authors/pages/dashboard.html', {
         'recipes': recipes,
+    })
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_edit(request, id):
+
+    recipe = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+        pk=id,
+    ).first()
+
+    if not recipe:
+        raise Http404()
+
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+        instance=recipe
+    )
+
+    if form.is_valid():
+        recipe = form.save(commit=False)
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+        recipe.save()
+
+        messages.success(request, 'Sua receita foi salva com sucesso!')
+        return redirect(reverse('authors:dashboard_recipe_edit', args=(id,)))
+
+    return render(request, 'authors/pages/dashboard_recipe.html', {
+        'form': form
     })
